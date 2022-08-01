35bc1451fb227f14af5b17fcf64dde99c0cec043
hueseyincelik <104273133+hueseyincelik@users.noreply.github.com>
2022-08-01 21:12:56 +0200

    allow more flexible serial port input

--- src/gui.py
+++ src/gui.py
@@ -22,9 +22,11 @@ class GUI:
 			for (label, tag, pos, callback, visibility) in zip(['CONNECT', 'START', 'STOP', 'SAVE'], ['connect_button', 'start_button', 'stop_button', 'save_button'], [[25,450], [625,450], [750,450], [875,450]], [self. initialize, self.start, self.stop, self.save], [True, False, False, False]):
 				dpg.add_button(label=label, tag=tag, width=100, height=50, pos=pos, callback=callback, show=visibility)
 
+			dpg.configure_item(item='connect_button', width=200)
+
 			with dpg.group(horizontal=True):
-				dpg.add_text('COM', tag='com_text', pos=[25, 510])
-				dpg.add_input_text(tag='com_port_input', no_spaces=True, decimal=True, width=55, default_value='4')
+				dpg.add_text('SERIAL PORT', tag='serial_port_text', pos=[25, 510])
+				dpg.add_input_text(tag='serial_port_input', no_spaces=True, width=91, default_value='COM4')
 
 				dpg.add_text('FILE NAME', tag='file_name', pos=[625, 510], show=False)
 				dpg.add_input_text(tag='file_name_input', no_spaces=True, width=260, default_value='data.txt', show=False)
@@ -46,7 +48,7 @@ class GUI:
 			dpg.bind_font(default_font)
 			dpg.set_global_font_scale(0.25)
 
-			for item in ['connect_button', 'start_button', 'stop_button', 'save_button', 'com_text', 'com_port_input', 'file_name', 'file_name_input', 'exciter_frequency', 'exciter_frequency_input', 'exciter_frequency_mHz']:
+			for item in ['connect_button', 'start_button', 'stop_button', 'save_button', 'serial_port_text', 'serial_port_input', 'file_name', 'file_name_input', 'exciter_frequency', 'exciter_frequency_input', 'exciter_frequency_mHz']:
 				dpg.bind_item_font(item, bold_font)
 
 		dpg.set_primary_window('main_window', True)
@@ -68,10 +70,10 @@ class GUI:
 
 	def initialize(self):
 		try:
-			self.com_port = dpg.get_value('com_port_input')
-			self.ardn = arduino.Arduino(f"COM{self.com_port}")
+			self.serial_port = dpg.get_value('serial_port_input')
+			self.ardn = arduino.Arduino(self.serial_port)
 		except:
-			self.popup_message('Arduino Connection', 'Error connecting to Arduino!\nCheck COM port!')
+			self.popup_message('Arduino Connection', 'Error connecting to Arduino!\nCheck serial port!')
 			self.change_item_visibility(['start_button', 'stop_button', 'save_button', 'file_name', 'file_name_input', 'exciter_frequency', 'exciter_frequency_input', 'exciter_frequency_mHz'], False)
 		else:
 			self.popup_message('Arduino Connection', 'Successfully connected to Arduino!')
@@ -112,7 +114,7 @@ class GUI:
 			try:
 				self.ardn
 			except:
-				self.ardn = arduino.Arduino(f"COM{self.com_port}")
+				self.ardn = arduino.Arduino(self.serial_port)
 
 			self.acquire_thread = Thread(target=self.acquire)
 			self.acquire_thread.start()
